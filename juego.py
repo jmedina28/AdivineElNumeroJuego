6276c862018e67211cb0fb5f3fa6c66b5a06b867
Juan Medina <juanperezmedina67@gmail.com>
2021-11-09 11:11:47 +0100

    hemos importado las librerías y creado la función que vamos a desarrollar posteriormente para la selección de nivel

---
+++ juego.py
@@ -0,0 +1,9 @@
+#Voy a volver a hacer el código sobre la marcha
+#con otro enfoque para que sea más facil introducir los nuevos cambios
+
+#importamos la librería que necesitamos
+import random
+
+#creamos una función 
+#para elegir el nivel en el futuro una vez vayamos a jugar
+def elegirnivel():
