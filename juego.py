08431a9ef6664fe9e982d3c15fc73b8669540d3b
Juan Medina <juanperezmedina67@gmail.com>
2021-11-09 11:16:27 +0100

    comprobación del correcto funcionamiento de la función creada

--- juego.py
+++ juego.py
@@ -7,3 +7,15 @@ import random
 #creamos una función 
 #para elegir el nivel en el futuro una vez vayamos a jugar
 def elegirnivel():
+    print("Seleccione un nivel de dificultad: ")
+    print("1 = Simple")
+    print("2 = Intermedio")
+    print("3 = Avanzado")
+    print("4 = Experto")
+    nivelelegido = int(input("¿En qué nivel de dificultad desea jugar?: "))
+    global eleccionnivel
+    eleccionnivel = nivelelegido
+    if eleccionnivel == 1 :
+        print("Usted ha seleccionado el nivel de dificultad simple")
+
+elegirnivel()
