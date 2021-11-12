b4f253577c6bccd200c7081454976b09d3cd96b0
Juan Medina <juanperezmedina67@gmail.com>
2021-11-12 08:45:54 +0100

    introduzco la variable maxintentos para poder crear la variable puntuacion y tener la tabla

--- juego.py
+++ juego.py
@@ -3,7 +3,8 @@
 
 #Importamos la librería que necesitamos.
 import random
-
+import tabulate
+from tabulate import tabulate
 #Creamos una función 
 #para elegir el nivel en el futuro una vez vayamos a jugar.
 def elegirnivel():
@@ -31,11 +32,12 @@ elegirnivel()
 
 #Ahora vamos a trabajar el nivel simple y en base a eso realizamos el resto de dificultades.
 if eleccionnivel == 1 :
+    maxintentos = 20
     nintentos = 0
     numero = random.randint(0,100)
     print(" \nA continuación se le va a pedir que adivine un número generado del 0 al 100.")
 #Este while nos limita a tener un número máximo de intentos, en este caso 20.
-    while nintentos < 20 :
+    while nintentos < maxintentos :
         print("Intente adivinar el número: ")
         intento = int(input())
         nintentos += 1
@@ -53,11 +55,12 @@ if eleccionnivel == 1 :
         print("Lo sentimos, no has logrado acertar el número en los 20 intentos que tenías.")
 
 if eleccionnivel == 2 :
+    maxintentos = 50
     nintentos = 0
     numero = random.randint(0,1000)
     print(" \nA continuación se le va a pedir que adivine un número generado del 0 al 1000.")
 #Este while nos limita a tener un número máximo de intentos, en este caso 50.
-    while nintentos < 50 :
+    while nintentos < maxintentos :
         print("Intente adivinar el número: ")
         intento = int(input())
         nintentos += 1
@@ -75,11 +78,12 @@ if eleccionnivel == 2 :
         print("Lo sentimos, no has logrado acertar el número en los 50 intentos que tenías.")
 
 if eleccionnivel == 3 :
+    maxintentos = 100
     nintentos = 0
     numero = random.randint(0,1000000)
     print(" \nA continuación se le va a pedir que adivine un número generado del 0 al 1000000.")
 #Este while nos limita a tener un número máximo de intentos, en este caso 100.
-    while nintentos < 100 :
+    while nintentos < maxintentos :
         print("Intente adivinar el número: ")
         intento = int(input())
         nintentos += 1
@@ -97,11 +101,12 @@ if eleccionnivel == 3 :
         print("Lo sentimos, no has logrado acertar el número en los 1000000 intentos que tenías.")
 
 if eleccionnivel == 4 :
+    maxintentos = 500
     nintentos = 0
     numero = random.randint(0,1000000000000)
     print(" \nA continuación se le va a pedir que adivine un número generado del 0 al 1000000000000.")
 #Este while nos limita a tener un número máximo de intentos, en este caso 500.
-    while nintentos < 500 :
+    while nintentos < maxintentos :
         print("Intente adivinar el número: ")
         intento = int(input())
         nintentos += 1
@@ -117,3 +122,6 @@ if eleccionnivel == 4 :
         print("Has logrado acertar el número con " + str(nintentos) + " intentos.")
     if intento != numero :
         print("Lo sentimos, no has logrado acertar el número en los 500 intentos que tenías.")
+puntuacion = maxintentos - nintentos
+nombre = str(input("Introduzca su nombre: "))
+tabla = [["Nombre", "Nivel de dificultad", "Puntuación"], [nombre, eleccionnivel, puntuacion]]
